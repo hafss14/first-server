de4454622849a37b866bbb360022161d03374ee5
hafss14 <66989571+hafss14@users.noreply.github.com>
2022-10-29 13:11:11 +0530

    todo-server

--- server.js
+++ server.js
@@ -2,15 +2,47 @@ const http = require("http");
 
 const port = 8081;
 
+const toDoList = ["complete Node Byte", "play cricket"];
+
 http
-    .createServer((request, response) => {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h1>Hello, this is from my server</h1>");
-        response.end();
+    .createServer((req, res) => {
+        const { method, url } = req;
+
+        if (url === "/todos") {
+            if (method === "GET") {
+                res.writeHead(200, { "Content-Type": "text/html " });
+                res.write(toDoList.toString());
+            }
+
+            else if (method === "POST") {
+                let body = "";
+                req
+                    .on("error", (err) => {
+                        console.error(err);
+                    })
+                    .on("data", (chunk) => {
+                        body += chunk;
+
+                    })
+                    .on("end", () => {
+                        body = JSON.parse(body);
+                        console.log("data: ", body);
+                    });
+            } else {
+                res.writeHead(501);
+            }
+        } else {
+            res.writeHead(404);
+        }
+
+
+        res.end();
+
     })
     .listen(port, () => {
         console.log(`Node.js server started o port ${port}`);
     });
+
 
 
     //http://localhost:8081
